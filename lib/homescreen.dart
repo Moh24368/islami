23b0039e2cf510032e62e0eb82dbd7f8f34dcb86
Moh24368 <M.Bassel2315@nu.edu.eg>
2026-09-10 00:26:44 +0300

    Add nav bar

---
+++ lib/homescreen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:islami/widgets/active_nav_bar_icon.dart';
+import 'package:islami/widgets/inactive_nav_bar_icon.dart';
+
+class Homescreen extends StatefulWidget {
+  static const String routeName = '/home';
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  int currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          currentIndex = index;
+          setState(() {});
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: "quran"),
+            activeIcon: ActiveNavBarIcon(iconName: "quran"),
+            label: "Quran",
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: "hadeth"),
+            activeIcon: ActiveNavBarIcon(iconName: "hadeth"),
+            label: "Hadeth",
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: "sebha"),
+            activeIcon: ActiveNavBarIcon(iconName: "sebha"),
+            label: "Sebha",
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: "radio"),
+            activeIcon: ActiveNavBarIcon(iconName: "radio"),
+            label: "Radio",
+          ),
+          BottomNavigationBarItem(
+            icon: InactiveNavBarIcon(iconName: "time"),
+            activeIcon: ActiveNavBarIcon(iconName: "time"),
+            label: "Time",
+          ),
+        ],
+      ),
+    );
+  }
+}
